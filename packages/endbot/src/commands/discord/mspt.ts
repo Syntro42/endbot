b7a5e62d1d27c695cf56653dd3cbd7acb1692044
Syntro42 <syntro42@gmail.com>
2021-07-26 22:38:02 -0500

    endbot: add embed class to send error embeds in a simpler way

--- packages/endbot/src/commands/discord/mspt.ts
+++ packages/endbot/src/commands/discord/mspt.ts
@@ -3,6 +3,7 @@ import { Bridge, Bridges } from "../../bridge/bridge";
 import { MessageEmbed } from "discord.js";
 import { Colors } from "../../utils/theme";
 import { command, discord, DiscordContext } from "../dispatcher";
+import { Embed } from "../../utils/embeds";
 
 @command(discord)
 export class MsptCommand
@@ -31,7 +32,7 @@ async function online(ctx: DiscordContext) {
     for (const bridge of bridges) {
         const result = await Mspt(bridge);
         if (isNaN(result.mspt)) {
-            await ctx.message.channel.send("That command requires carpet mod on the server!")
+            await ctx.message.channel.send(Embed.error("That command requires carpet mod on the server!"));
             return;
         }
 
