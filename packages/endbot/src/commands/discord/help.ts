b806d21f2d27bd2c5dbc79805f18eaef3435ee0f
Syntro42 <syntro42@gmail.com>
2021-08-02 22:55:38 -0500

    endbot: fix indenting for discord help

--- packages/endbot/src/commands/discord/help.ts
+++ packages/endbot/src/commands/discord/help.ts
@@ -19,7 +19,7 @@ class HelpCommand
 async function discordHelp(ctx: DiscordContext) {
 	const command = ctx.args[1]
 	const help = await getCommand(command)
-	if (help == null) return getHelp(ctx);
+	if (!help) return getHelp(ctx);
 
 	const embed = new MessageEmbed()
 		.setColor(Colors.ENDTECH)
@@ -40,9 +40,8 @@ async function getHelp(ctx: DiscordContext) {
 
 	for (const key in commands) {
 		embed.addField(
-			commands[key].name,
-			`${commands[key].description}\n \`${commands[key].usage}\``,
-			false
+			`**${commands[key].name}**`,
+			`${commands[key].description}\n \`${commands[key].usage}\``
 		);
 	}
 	return ctx.message.channel.send(embed);
@@ -50,70 +49,70 @@ async function getHelp(ctx: DiscordContext) {
 
 const commands: {[command: string]: commandInfo} = {
 	help:
-		{
+	{
 		name: "Help command",
 		usage: "help [command]",
 		description: "Get help with a command"
 	},
 
 	backup:
-		{
+	{
 		name: "Backup command",
 		usage: "backup [servername]",
 		description: "Creates a backup for the server"
 	},
 
 	embed:
-		{
+	{
 		name: "Embed editor",
 		usage: "embed create",
 		description: "Create and edit embeds"
 	},
 
 	execute:
-		{
+	{
 		name: "Execute command",
 		usage: "execute <command>",
 		description: "Execute a command on a server from a bridge channel"
 	},
 
 	links:
-		{
+	{
 		name: "Discord Links",
 		usage: "links add",
 		description: "Add discord invitations to an embed"
 	},
 
 	list:
-		{
+	{
 		name: "List command",
 		usage: "list [role]",
 		description: "Lists all people with the specified role"
 	},
 
 	mspt:
-		{
+	{
 		name: "MSPT command",
 		usage: "mspt [servername]",
 		description: "Gets the TPS and MSPT the server is running at"
 	},
 
 	online:
-		{
+	{
 		name: "Online command",
 		usage: "online [servername]",
 		description: "Gets a list of all online players on the server"
 	},
 
 	pos:
-		{
+	{
 		name: "Pos command",
 		usage: "pos <playername>",
 		description: "Gets the position of a player"
 	},
 
 	scoreboard:
-		{
+	{
 		name: "Scoreboard command",
 		usage: "scoreboard <objectives> [flag]",
 		description: "Displays a scoreboard of the specified objective",
